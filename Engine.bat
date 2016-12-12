2b0bd1590dcd5d095affc0ea55e0a92ce7ef8b0e
99max99 <emrosswarone@gmail.com>
2016-12-12 10:00:52 +0800

    Update Engine.bat

--- Engine.bat
+++ Engine.bat
@@ -1,5 +1,5 @@
 @echo off
 title Engine
 echo Launching Server...
-"C:/Program Files/Java/jre1.8.0_111/bin/java.exe" -Xmx512m -cp bin;library/*; net.kagani.Engine 1 false true
+"C:/Program Files/Java/jre1.8.0_111/bin/java.exe" -Xmx512m -cp bin;library/*; net.kagani.Engine 1 true false
 pause
